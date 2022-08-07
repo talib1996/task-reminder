e02f7ccff4df1d319439ad8fba1b01f1686080d8
Talib <fahadimtiaz46@gmail.com>
2022-08-08 02:23:11 +0500

    added some code

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,13 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        /*
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-          //  'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
+*/
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
